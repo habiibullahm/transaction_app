4da14ba332af93fcdc6688d83485c38d1a9f7f57
Muhammad Habibullah <Muhammadhabibullah32@gmail.com>
2022-03-30 07:24:09 +0700

    fix routes

--- routes/transaction.js
+++ routes/transaction.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 router.get("/getAll", getTransactions);
 router.post("/create", isAdmin, createTransaction);
-router.get("/getOne/detail", isAdmin, getTransaction);
+router.get("/getOne/detail", getTransaction);
 
 module.exports = router;
